1484437a7a2180cb4283a67da4d21558f7de76e1
itaybnv <itaybnv01@gmail.com>
2019-10-30 23:52:07 +0200

    Implement a simple search handler

---
+++ server/server/Video.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    class Video
+    {
+        public string Title { get; }
+        public string ChannelTitle { get; }
+        public string Id { get; }
+        public string ThumbnailUrl { get; }
+
+        public Video(string title, string channelTitle, string id, string thumbnailUrl)
+        {
+            this.Title = title;
+            this.ChannelTitle = channelTitle;
+            this.Id = id;
+            this.ThumbnailUrl = thumbnailUrl;
+        }
+    }
+}
---
+++ server/server/search.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
+
+namespace server
+{
+    class Search
+    {
+        private YouTubeService youtubeService;
+
+        public Search()
+        {
+            // Create a connection with youtube service
+            youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyDSS76wLt0XodIC9SGx9gxH5EOhzUe5dII",
+                ApplicationName = this.GetType().ToString()
+            });
+        }
+
+        public List<Video> ByTitle(string title, int resultAmount)
+        {
+            List<Video> resultList = new List<Video>();
+
+            // Create a new request for the search query
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = title;
+            searchListRequest.MaxResults = resultAmount;
+            // Limit the results to video only
+            searchListRequest.Type = "video";
+
+            // Execute search request
+            var searchListResponse = searchListRequest.Execute();
+
+            // Instantiate a video object for each search result and add
+            // it to the result list
+            foreach (var searchResult in searchListResponse.Items)
+            {
+                if(searchResult.Id.Kind == "youtube#video")
+                {
+                    resultList.Add(new Video(searchResult.Snippet.Title,
+                                             searchResult.Snippet.ChannelTitle,
+                                             searchResult.Id.VideoId,
+                                             searchResult.Snippet.Thumbnails.Default__.Url));
+                }
+            }
+
+            return resultList;
+        }
+    }
+}
