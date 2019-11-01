7ffd72045ae9bd51d59e2f3a160a71fc33e21c36
itaybnv <itaybnv01@gmail.com>
2019-11-01 21:20:53 +0200

    Add abstract class of packet

---
+++ server/server/Packet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace server
+{
+    enum PacketType
+    {
+        getConnectedUsers,
+    }
+
+    abstract class Packet
+    {
+        private PacketType type;
+        protected string data;
+        
+        public Packet(PacketType type)
+        {
+            this.type = type;
+        }
+
+        public abstract Packet Execute()
+        {
+
+        };
+    }
+}
