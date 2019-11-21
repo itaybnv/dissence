50bdd93d186f288ad23abf28c2d64f1e631eb6cf
Almog Hamdani <almogh52@gmail.com>
2019-11-21 17:36:50 +0200

    add ticker

--- client/src/components/DissencePlaylistItem.jsx
+++ client/src/components/DissencePlaylistItem.jsx
@@ -4,12 +4,15 @@ import { ListItem } from "@rmwc/list";
 import { Typography } from "@rmwc/typography";
 import { SimpleMenu, MenuItem } from "@rmwc/menu";
 
+import Ticker from "react-ticker";
+
 import "./DissencePlaylistItem.scss";
 
 export default class DissencePlaylistItem extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			textScroll: false,
 			videoThumbnailUrl: props.videoThumbnailUrl
 				? props.videoThumbnailUrl
 				: "https://onaliternote.files.wordpress.com/2016/11/wp-1480230666843.jpg",
@@ -19,14 +22,23 @@ export default class DissencePlaylistItem extends Component {
 
 	getListItem = () => {
 		return (
-			<ListItem>
+			<ListItem
+				onMouseEnter={() => this.setState({ textScroll: true })}
+				onMouseLeave={() => this.setState({ textScroll: false })}
+			>
 				<img
 					className="dissence-playlist-list-item-image"
 					src={this.state.videoThumbnailUrl}
 					alt="image error"
 				></img>
-				<Typography className="dissence-playlist-list-item-title" use="body2">
-					{this.state.videoTitle}
+				<Typography
+					className="dissence-playlist-list-item-title"
+					use="body2"
+					style={{ width: "100%" }}
+				>
+					<Ticker move={this.state.textScroll}>
+						{() => <span>{this.state.videoTitle}</span>}
+					</Ticker>
 				</Typography>
 			</ListItem>
 		);
