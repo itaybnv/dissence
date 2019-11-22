db8a3c88b1fdcc08dcd2e3387aae2bf11a139453
itaybnv <itaybnv01@gmail.com>
2019-11-22 09:20:52 +0200

    Refactor dissence toolbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,68 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
 
-import DissenceToolbar from "./components/DissenceToolbar";
 import DissenceUsersList from "./components/DissenceUsersList";
 import DissencePlaylist from "./components/DissencePlaylist";
 import DissenceControlBar from "./components/DissenceControlBar";
 import DissenceSearchContent from "./components/DissenceSearchContent";
 
-function App() {
-  return (
-    <div className="dissence-app-container">
-      <div className="dissence-header-container">
-        <DissenceToolbar />
-      </div>
+import {
+	TopAppBar,
+	TopAppBarFixedAdjust,
+	TopAppBarRow,
+	TopAppBarSection,
+	TopAppBarTitle
+} from "@rmwc/top-app-bar";
+import { TextField } from "@rmwc/textfield";
 
-      <div className="dissence-main-container">
-        <DissencePlaylist />
-        <DissenceSearchContent />
-        <DissenceUsersList />
-      </div>
-      <div className="dissence-footer-container">
-        <DissenceControlBar />
-      </div>
-    </div>
-  );
+import "./App.scss";
+
+import searchController from "./controllers/SearchController";
+
+class App extends Component {
+	state = { searchValue: "", results: [] };
+
+	onSearch = evt => {
+		evt.preventDefault();
+		searchController.ByTitle(this.state.searchValue).then(results => {
+			this.setState({ results: results.results });
+		});
+	};
+
+	render() {
+		return (
+			<div className="dissence-app-container">
+				<div className="dissence-header-container">
+					<TopAppBar fixed>
+						<TopAppBarRow>
+							<TopAppBarSection alignStart>
+								<TopAppBarTitle>Dissence</TopAppBarTitle>
+								<form onSubmit={this.onSearch}>
+									<TextField
+										className="dissence-video-search-field"
+										outlined
+										placeholder="Search video.."
+										value={this.state.searchValue}
+										onChange={evt =>
+											this.setState({ searchValue: evt.target.value })
+										}
+									/>
+								</form>
+							</TopAppBarSection>
+						</TopAppBarRow>
+					</TopAppBar>
+					<TopAppBarFixedAdjust />
+				</div>
+				<div className="dissence-main-container">
+					<DissencePlaylist />
+					<DissenceSearchContent searchResults={this.state.results} />
+					<DissenceUsersList />
+				</div>
+				<div className="dissence-footer-container">
+					<DissenceControlBar />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
