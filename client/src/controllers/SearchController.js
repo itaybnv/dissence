6aef09d205c1f47f8c9540e308b99b9e37265729
itaybnv <itaybnv01@gmail.com>
2019-11-24 19:01:11 +0200

    Implement HTML and XML entities decode #17

--- client/src/controllers/SearchController.js
+++ client/src/controllers/SearchController.js
@@ -1,5 +1,6 @@
 import networkController from "../networking/NetworkController";
 import PacketType from "../misc/PacketType";
+const Entities = require("html-entities").AllHtmlEntities;
 
 class SearchController {
 	ByTitle = query =>
@@ -8,7 +9,16 @@ class SearchController {
 			let dataBuffer = Buffer.from(JSON.stringify(data));
 			networkController
 				.send(dataBuffer, PacketType.GET_SEARCH_RESULT)
-				.then(res => resolve(JSON.parse(res.data.toString())));
+				.then(res => {
+					const entities = new Entities();
+
+					let data = JSON.parse(res.data.toString())
+					for (let i = 0; i < data.results.length - 1; i++) {
+						data.results[i].Title = entities.decode(data.results[i].Title);
+					}
+
+					resolve(data);
+				});
 		});
 }
 
